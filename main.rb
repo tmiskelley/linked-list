c38593a61b3b6e5f81238f8e13f025184003d4eb
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-05-31 07:52:26 -0400

    Added "prepend" and "size" methods to Linked List class

--- main.rb
+++ main.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# Models a singly linked list data structure
 class LinkedList
   attr_accessor :head, :tail
 
@@ -20,14 +21,34 @@ class LinkedList
       @tail = new_node
     end
   end
+
+  def prepend(value)
+    new_node = Node.new(value)
+    previous_head = @head
+    return @head = new_node if @head.nil?
+
+    @head = new_node
+    @head.next_node = previous_head
+    @tail = previous_head
+  end
+
+  def size
+    puts Node.count
+  end
 end
 
+# Models node objects for linked list
 class Node
-
+  @@count = 0
   attr_accessor :next_node
 
   def initialize(data)
     @data = data
     @next_node = nil
+    @@count += 1
+  end
+
+  def self.count
+    @@count
   end
 end
